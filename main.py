5c651647d6bb2e4f84f7939331a9cae19dbfcec2
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2022-12-10 09:38:25 +0100

    20221209 - new random get

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import random
 app = FastAPI()
 
 
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
@@ -18,7 +17,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-engine = db.create_engine('sqlite:///wisielec.db', connect_args={"check_same_thread": False})
+engine = db.create_engine('sqlite:///hangman.db', connect_args={"check_same_thread": False})
 connection = engine.connect()
 metadata = db.MetaData()
 
@@ -40,20 +39,74 @@ metadata.create_all(engine)
 print("Klucze tabeli Categories:", categories.columns.keys(), '\n')
 print("Klucze tabeli Words:", words.columns.keys(), '\n')
 
+# dodaję dane do tabeli Categories
+
+# query = db.insert(categories).values(id=1, name='Przyroda', description="Kategoria przyroda")
+# connection.execute(query)
+# select_users_query = db.select([categories])
+# select_results = connection.execute(select_users_query)
+# print("\n", select_results.fetchall())
+
+# Pobranie wszystkich kategorii
+
+
 @app.get("/categories")
 def get_categories():
     try:
         query = db.select([categories])
         result = (connection.execute(query)).fetchall()
-        if not result:
+        if not result: # if result = []
             return {"status": "failed", "info": "no records"}
         return result
     except Exception as error:
         print(error)
         return {"status": "failed"}
 
+# Pobranie jednej kategorii
 
-@app.get("/words/random")
-def get_words_random():
-    query = random(db.select([words]))
-    result = (connection.execute(query)).fetchall()
+
+@app.get("/categories/{id}")
+def get_category_by_id(id: int):
+    try:
+        query = db.select([categories]).where(categories.columns.id == id)
+        result = (connection.execute(query)).fetchone()
+        print(type(result))
+        if not result: # if result is None
+            return {"status": "failed", "info": "no category with this id"}
+        return result
+    except Exception as error:
+        print(error)
+        return {"status": "failed"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @app.get("/words/random")
+# def get_words_random():
+#     try:
+#         query = db.select([words])
+#         result = (connection.execute(query)).fetchall()
+#         if not result:
+#             return {"status": "failed", "info": "no records"}
+#         return result
+#     except Exception as error:
+#         print(error)
+#         return {"status": "failed"}
+#
+# @app.get("/categories/{id}/word/")
+# def get_specific_word(categories_id: int):
+#     query = db.select([categories, words])
+#     query = query.select_from(categories.join(words, categories.columns.id == words.columns.category_id))
+#     result = (connection.execute(query)).fetchall()
+#     print(result)
